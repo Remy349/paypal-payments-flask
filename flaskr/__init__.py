48eb1228ef1a1aa8eb85c84b41d8190122c580ee
Remy349 <santoraga15@gmail.com>
2023-05-25 21:03:05 -0600

    Added basic template and route.

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,7 +1,13 @@
 from flask import Flask
+from config import Config
 
 
-def create_app():
+def create_app(class_config=Config):
     app = Flask(__name__)
+
+    app.config.from_object(class_config)
+
+    from flaskr.main.routes import bp as main_bp
+    app.register_blueprint(main_bp)
 
     return app
