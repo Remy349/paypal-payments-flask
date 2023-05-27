2848089205a82c430608052ddb1232e2c4068a69
Remy349 <santoraga15@gmail.com>
2023-05-27 12:02:28 -0600

    Refactor code and create a function to get the paypal access token.

--- flaskr/main/routes.py
+++ flaskr/main/routes.py
@@ -1,6 +1,7 @@
 import os
 import requests
-from flask import Blueprint, jsonify, render_template, url_for
+import base64
+from flask import Blueprint, jsonify, render_template
 
 bp = Blueprint("main", __name__)
 
@@ -12,69 +13,24 @@ def index():
 
 @bp.route("/create-order", methods=["GET"])
 def create_order():
-    client = os.getenv("PAYPAL_API_CLIENT")
-    secret = os.getenv("PAYPAL_API_SECRET")
+    api_url = os.getenv("PAYPAL_API_URL")
+    client_id = os.getenv("PAYPAL_CLIENT_ID")
+    client_secret = os.getenv("PAYPAL_CLIENT_SECRET")
 
-    data = {
-        "intent": "CAPTURE",
-        "purchase_units": [
-            {
-                "amount": {
-                    "currency_code": "USD",
-                    "value": "100.00",
-                },
-                "items": [
-                    {
-                        "name": "Test",
-                        "description": "Test para una compra usando la RESTApi de PayPal",
-                        "quantity": "1",
-                        "unit_amount": {
-                            "currency_code": "USD",
-                            "value": "100.00",
-                        },
-                    }
-                ],
-            }
-        ],
-        "payment_source": {
-            "paypal": {
-                "experience_context": {
-                    "brand_name": "Mi tienda de prueba",
-                    "landing_page": "NO_PREFERENCE",
-                    "user_action": "PAY_NOW",
-                    "return_url": "/capture-order",
-                    "cancel_url": "/cancel-order",
-                }
-            }
-        },
-    }
+    access_token = get_access_token(api_url, client_id, client_secret)
 
-    response = requests.post(
-        f"{os.getenv('PAYPAL_API_URL')}/v1/oauth2/token",
-        headers={
-            "Content-Type": "application/x-www-form-urlencoded",
-            "Accept": "application/json",
-        },
-        auth=(client, secret),
-        data={"grant_type": "client_credentials"},
-    )
+    # order_response = requests.post(
+    #     f"{os.getenv('PAYPAL_API_URL')}/v2/checkout/orders",
+    #     data=data,
+    #     headers={
+    #         "Content-Type": "application/json",
+    #         "Authorization": f"Bearer {access_token}",
+    #     },
+    # )
 
-    response_json = response.json()
+    # print(order_response.json())
 
-    access_token = response_json["access_token"]
-
-    order_response = requests.post(
-        f"{os.getenv('PAYPAL_API_URL')}/v2/checkout/orders",
-        data=data,
-        headers={
-            "Content-Type": "application/json",
-            "Authorization": f"Bearer {access_token}",
-        },
-    )
-
-    print(order_response.content)
-
-    return jsonify(data)
+    return jsonify("create-order")
 
 
 @bp.route("/capture-order", methods=["GET"])
@@ -85,3 +41,28 @@ def capture_order():
 @bp.route("/cancel-order", methods=["GET"])
 def cancel_order():
     return "Cancel Order"
+
+
+def get_access_token(api_url, client_id, client_secret):
+    basic_auth = base64.b64encode(
+        f"{client_id}:{client_secret}".encode(),
+    ).decode()
+
+    data = {
+        "grant_type": "client_credentials",
+        "client_id": client_id,
+        "client_secret": client_secret,
+    }
+
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Authorization": f"Basic {basic_auth}",
+    }
+
+    response = requests.post(
+        f"{api_url}/v1/oauth2/token",
+        headers=headers,
+        data=data,
+    ).json()
+
+    return response["access_token"]
